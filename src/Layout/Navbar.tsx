b87b5aeff2efab1564f54daa5543aa67ceeef3ac
yoosaemsol <yoosaemsol@gmail.com>
2023-09-14 14:44:42 +0900

    feat: Add home logo to the Navbar

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -5,6 +5,9 @@ import styles from './Navbar.module.css';
 export default function Navbar() {
   return (
     <header className={styles.header}>
+      <Link className={styles.logo} to="/">
+        Blog
+      </Link>
       <nav>
         <Link to="/posts/new">Write</Link>
         <Link to="/posts">Post</Link>
