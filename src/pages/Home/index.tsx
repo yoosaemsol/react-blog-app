c3b32451b061f434af068fd23e8a752d7d96e8c3
yoosaemsol <yoosaemsol@gmail.com>
2023-09-14 17:14:54 +0900

    feat: Add banner to the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Page } from 'components/ui';
 import PostList from 'components/PostList';
+import Banner from 'components/Banner';
 
 export default function Home() {
   return (
     <Page>
+      <Banner />
       <PostList />
     </Page>
   );
