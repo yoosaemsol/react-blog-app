58f519547230881a70ebdd50575fdf33c1c020c5
yoosaemsol <yoosaemsol@gmail.com>
2023-09-14 19:17:38 +0900

    feat: Set active state for filter buttons in PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,18 +1,40 @@
-import styles from './PostList.module.css';
+import { useState } from 'react';
 
 import PostBox from 'components/PostBox';
+
+import styles from './PostList.module.css';
 
 interface PostListProps {
   onFilter?: boolean;
 }
 
+type FilterType = 'all' | 'my';
+
 export default function PostList({ onFilter = true }: PostListProps) {
+  const [activeFilter, setActiveFilter] = useState<FilterType>('all');
+
   return (
     <section>
       {onFilter && (
         <ul className={styles.filter}>
-          <li className={`${styles.filterItem}  ${styles.active}`}>All</li>
-          <li className={styles.filterItem}>My Posts</li>
+          <li
+            role="presentation"
+            className={`${styles.filterItem}  ${
+              activeFilter === 'all' && styles.active
+            }`}
+            onClick={() => setActiveFilter('all')}
+          >
+            All
+          </li>
+          <li
+            role="presentation"
+            className={`${styles.filterItem}  ${
+              activeFilter === 'my' && styles.active
+            }`}
+            onClick={() => setActiveFilter('my')}
+          >
+            My Posts
+          </li>
         </ul>
       )}
       <ul className={styles.postList}>
