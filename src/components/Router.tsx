0ce0d2dce17a266274aaf42c9b452d34771f4073
yoosaemsol <yoosaemsol@gmail.com>
2023-09-14 22:53:18 +0900

    feat: Implement routing based on authentication

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from 'pages/Home';
 import Posts from 'pages/Posts';
@@ -10,18 +11,31 @@ import Signup from 'pages/Signup';
 import Layout from 'Layout/Layout';
 
 export default function Router() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="posts" element={<Posts />} />
-        <Route path="posts/:id" element={<Detail />} />
-        <Route path="posts/new" element={<New />} />
-        <Route path="posts/edit/:id" element={<Edit />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route index element={<Home />} />
+            <Route path="posts" element={<Posts />} />
+            <Route path="posts/:id" element={<Detail />} />
+            <Route path="posts/new" element={<New />} />
+            <Route path="posts/edit/:id" element={<Edit />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route index element={<Login />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        )}
       </Route>
     </Routes>
   );
