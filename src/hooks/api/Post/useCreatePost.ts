29358bd367eb03a66872eb3fb2cbabcf280f3d4b
yoosaemsol <yoosaemsol@gmail.com>
2023-09-17 18:54:05 +0900

    feat: Include time info in Post createdAt field

--- src/hooks/api/Post/useCreatePost.ts
+++ src/hooks/api/Post/useCreatePost.ts
@@ -18,7 +18,7 @@ export const useCreatePost = (
         title,
         summary,
         content,
-        createdAt: new Date()?.toLocaleDateString(),
+        createdAt: new Date()?.toLocaleString(),
         email,
       });
 
