a756bbc27c9746bfb4e00097b43a9b464beb8599
yoosaemsol <yoosaemsol@gmail.com>
2023-09-19 02:36:20 +0900

    feat: Add dark theme toggle functionality

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
+import { BsSun, BsMoonFill } from 'react-icons/bs';
+import { useDarkModeContext } from 'context/DarkModeContext';
 
 export default function Footer() {
+  const { darkMode, toggleDarkMode } = useDarkModeContext();
+
   return (
     <footer className={styles.footer}>
-      <Link to="/posts/new">Write</Link>
-      <Link to="/posts">Post</Link>
-      <Link to="/profile">Profile</Link>
+      <div className={styles.menu}>
+        <Link to="/posts/new">Write</Link>
+        <Link to="/posts">Post</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+      <div className={styles.themeBtn} onClick={() => toggleDarkMode()}>
+        {darkMode ? <BsMoonFill /> : <BsSun />}
+      </div>
     </footer>
   );
 }
